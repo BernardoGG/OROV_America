3cf6fb4cc59e3927111d1927c141bd725c44ef69
BernardoGG <bernardo.gutierrez@zoo.ox.ac.uk>
2019-02-12 16:40:05 +0000

    A commit from RStudio (MacPro)

--- OROV_SimPlots.R
+++ OROV_SimPlots.R
@@ -94,186 +94,186 @@ length(S8_wndws)
 L1_dist <- sapply(L1_wndws, dist.dna)
 x <- length(L1_wndws)
 L1_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = L1_dist
 )
 L2_dist <- sapply(L2_wndws, dist.dna)
 x <- length(L2_wndws)
 L2_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = L2_dist
 )
 L3_dist <- sapply(L3_wndws, dist.dna)
 x <- length(L3_wndws)
 L3_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = L3_dist
 )
 L4_dist <- sapply(L4_wndws, dist.dna)
 x <- length(L4_wndws)
 L4_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = L4_dist
 )
 L5_dist <- sapply(L5_wndws, dist.dna)
 x <- length(L5_wndws)
 L5_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = L5_dist
 )
 L6_dist <- sapply(L6_wndws, dist.dna)
 x <- length(L6_wndws)
 L6_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = L6_dist
 )
 L7_dist <- sapply(L7_wndws, dist.dna)
 x <- length(L7_wndws)
 L7_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = L7_dist
 )
 L8_dist <- sapply(L8_wndws, dist.dna)
 x <- length(L8_wndws)
 L8_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = L8_dist
 )
 
 G1a_dist <- sapply(G1a_wndws, dist.dna)
 x <- length(G1a_wndws)
 G1a_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G1a_dist
 )
 G1b_dist <- sapply(G1b_wndws, dist.dna)
 x <- length(G1b_wndws)
 G1b_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G1b_dist
 )
 G1c_dist <- sapply(G1c_wndws, dist.dna)
 x <- length(G1c_wndws)
 G1c_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G1c_dist
 )
 G1d_dist <- sapply(G1d_wndws, dist.dna)
 x <- length(G1d_wndws)
 G1d_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G1d_dist
 )
 G2a_dist <- sapply(G2a_wndws, dist.dna)
 x <- length(G2a_wndws)
 G2a_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G2a_dist
 )
 G2b_dist <- sapply(G2b_wndws, dist.dna)
 x <- length(G2b_wndws)
 G2b_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G2b_dist
 )
 G2c_dist <- sapply(G2c_wndws, dist.dna)
 x <- length(G2c_wndws)
 G2c_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G2c_dist
 )
 G2d_dist <- sapply(G2d_wndws, dist.dna)
 x <- length(G2d_wndws)
 G2d_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G2d_dist
 )
 
 S1_dist <- sapply(S1_wndws, dist.dna)
 x <- length(S1_wndws)
 S1_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = S1_dist
 )
 S2_dist <- sapply(S2_wndws, dist.dna)
 x <- length(S2_wndws)
 S2_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = S2_dist
 )
 S3_dist <- sapply(S3_wndws, dist.dna)
 x <- length(S3_wndws)
 S3_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = S3_dist
 )
 S4_dist <- sapply(S4_wndws, dist.dna)
 x <- length(S4_wndws)
 S4_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = S4_dist
 )
 S5_dist <- sapply(S5_wndws, dist.dna)
 x <- length(S5_wndws)
 S5_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = S5_dist
 )
 S6_dist <- sapply(S6_wndws, dist.dna)
 x <- length(S6_wndws)
 S6_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = S6_dist
 )
 S7_dist <- sapply(S7_wndws, dist.dna)
 x <- length(S7_wndws)
 S7_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = S7_dist
 )
 S8_dist <- sapply(S8_wndws, dist.dna)
 x <- length(S8_wndws)
 S8_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = S8_dist
 )
 
 #Plot genetic distances for each segment
 ggplot() +
-  geom_line(data = L1_dist, aes(x = Window, y = Dist), color = "olivedrab") +
-  geom_line(data = L2_dist, aes(x = Window, y = Dist), color = "olivedrab4") +
-  geom_line(data = L3_dist, aes(x = Window, y = Dist), color = "olivedrab3") +
-  geom_line(data = L4_dist, aes(x = Window, y = Dist), color = "olivedrab2") +
-  geom_line(data = L5_dist, aes(x = Window, y = Dist), color = "palegreen4") +
-  geom_line(data = L6_dist, aes(x = Window, y = Dist), color = "palegreen3") +
-  geom_line(data = L7_dist, aes(x = Window, y = Dist), color = "palegreen2") +
-  geom_line(data = L8_dist, aes(x = Window, y = Dist), color = "palegreen1") +
+  geom_line(data = L1_dist, aes(x = Window.Start, y = Dist), color = "olivedrab") +
+  geom_line(data = L2_dist, aes(x = Window.Start, y = Dist), color = "olivedrab4") +
+  geom_line(data = L3_dist, aes(x = Window.Start, y = Dist), color = "olivedrab3") +
+  geom_line(data = L4_dist, aes(x = Window.Start, y = Dist), color = "olivedrab2") +
+  geom_line(data = L5_dist, aes(x = Window.Start, y = Dist), color = "palegreen4") +
+  geom_line(data = L6_dist, aes(x = Window.Start, y = Dist), color = "palegreen3") +
+  geom_line(data = L7_dist, aes(x = Window.Start, y = Dist), color = "palegreen2") +
+  geom_line(data = L8_dist, aes(x = Window.Start, y = Dist), color = "palegreen1") +
   ylim(0, 0.7) +
   xlab("L") +
   ylab("Distance to IQTV")
 
 ggplot() +
-  geom_line(data = G1a_dist, aes(x = Window, y = Dist), color = "royalblue4") +
-  geom_line(data = G1b_dist, aes(x = Window, y = Dist), color = "royalblue3") +
-  geom_line(data = G1c_dist, aes(x = Window, y = Dist), color = "royalblue") +
-  geom_line(data = G1d_dist, aes(x = Window, y = Dist), color = "royalblue1") +
-  geom_line(data = G2a_dist, aes(x = Window, y = Dist), color = "darkred") +
-  geom_line(data = G2b_dist, aes(x = Window, y = Dist), color = "firebrick") +
-  geom_line(data = G2c_dist, aes(x = Window, y = Dist), color = "firebrick3") +
-  geom_line(data = G2d_dist, aes(x = Window, y = Dist), color = "firebrick1") +
+  geom_line(data = G1a_dist, aes(x = Window.Start, y = Dist), color = "royalblue4") +
+  geom_line(data = G1b_dist, aes(x = Window.Start, y = Dist), color = "royalblue3") +
+  geom_line(data = G1c_dist, aes(x = Window.Start, y = Dist), color = "royalblue") +
+  geom_line(data = G1d_dist, aes(x = Window.Start, y = Dist), color = "royalblue1") +
+  geom_line(data = G2a_dist, aes(x = Window.Start, y = Dist), color = "darkred") +
+  geom_line(data = G2b_dist, aes(x = Window.Start, y = Dist), color = "firebrick") +
+  geom_line(data = G2c_dist, aes(x = Window.Start, y = Dist), color = "firebrick3") +
+  geom_line(data = G2d_dist, aes(x = Window.Start, y = Dist), color = "firebrick1") +
   ylim(0, 0.7) +
   xlab("M") +
   ylab("Distance to IQTV")
 
 ggplot() +
-  geom_line(data = S1_dist, aes(x = Window, y = Dist), color = "goldenrod4") +
-  geom_line(data = S2_dist, aes(x = Window, y = Dist), color = "goldenrod3") +
-  geom_line(data = S3_dist, aes(x = Window, y = Dist), color = "goldenrod2") +
-  geom_line(data = S4_dist, aes(x = Window, y = Dist), color = "goldenrod1") +
-  geom_line(data = S5_dist, aes(x = Window, y = Dist), color = "lightgoldenrod4") +
-  geom_line(data = S6_dist, aes(x = Window, y = Dist), color = "lightgoldenrod3") +
-  geom_line(data = S7_dist, aes(x = Window, y = Dist), color = "lightgoldenrod2") +
-  geom_line(data = S8_dist, aes(x = Window, y = Dist), color = "lightgoldenrod1") +
+  geom_line(data = S1_dist, aes(x = Window.Start, y = Dist), color = "goldenrod4") +
+  geom_line(data = S2_dist, aes(x = Window.Start, y = Dist), color = "goldenrod3") +
+  geom_line(data = S3_dist, aes(x = Window.Start, y = Dist), color = "goldenrod2") +
+  geom_line(data = S4_dist, aes(x = Window.Start, y = Dist), color = "goldenrod1") +
+  geom_line(data = S5_dist, aes(x = Window.Start, y = Dist), color = "lightgoldenrod4") +
+  geom_line(data = S6_dist, aes(x = Window.Start, y = Dist), color = "lightgoldenrod3") +
+  geom_line(data = S7_dist, aes(x = Window.Start, y = Dist), color = "lightgoldenrod2") +
+  geom_line(data = S8_dist, aes(x = Window.Start, y = Dist), color = "lightgoldenrod1") +
   ylim(0, 0.7) +
   xlab("S") +
   ylab("Distance to IQTV")
@@ -326,91 +326,91 @@ length(G2OUTc_wndws)
 G1INa_dist <- sapply(G1INa_wndws, dist.dna)
 x <- length(G1INa_wndws)
 G1INa_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G1INa_dist
 )
 G1INb_dist <- sapply(G1INb_wndws, dist.dna)
 x <- length(G1INb_wndws)
 G1INb_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G1INb_dist
 )
 G1INc_dist <- sapply(G1INc_wndws, dist.dna)
 x <- length(G1INc_wndws)
 G1INc_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G1INc_dist
 )
 G1OUTa_dist <- sapply(G1OUTa_wndws, dist.dna)
 x <- length(G1OUTa_wndws)
 G1OUTa_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G1OUTa_dist
 )
 G1OUTb_dist <- sapply(G1OUTb_wndws, dist.dna)
 x <- length(G1OUTb_wndws)
 G1OUTb_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G1OUTb_dist
 )
 G1OUTc_dist <- sapply(G1OUTc_wndws, dist.dna)
 x <- length(G1OUTc_wndws)
 G1OUTc_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G1OUTc_dist
 )
 G2INa_dist <- sapply(G2INa_wndws, dist.dna)
 x <- length(G2INa_wndws)
 G2INa_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G2INa_dist
 )
 G2INb_dist <- sapply(G2INb_wndws, dist.dna)
 x <- length(G2INb_wndws)
 G2INb_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G2INb_dist
 )
 G2INc_dist <- sapply(G2INc_wndws, dist.dna)
 x <- length(G2INc_wndws)
 G2INc_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G2INc_dist
 )
 G2OUTa_dist <- sapply(G2OUTa_wndws, dist.dna)
 x <- length(G2OUTa_wndws)
 G2OUTa_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G2OUTa_dist
 )
 G2OUTb_dist <- sapply(G2OUTb_wndws, dist.dna)
 x <- length(G2OUTb_wndws)
 G2OUTb_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G2OUTb_dist
 )
 G2OUTc_dist <- sapply(G2OUTc_wndws, dist.dna)
 x <- length(G2OUTc_wndws)
 G2OUTc_dist <- data.frame(
-  Window = c(seq(20, 20*x, by = 20)),
+  Window.Start = c(seq(1, 20*x, by = 20)),
   Dist = G2OUTc_dist
 )
 
 #SimPlots
 ggplot() +
-  geom_line(data = G1INa_dist, aes(x = Window, y = Dist), color = "red") +
-  geom_line(data = G1INb_dist, aes(x = Window, y = Dist), color = "red") +
-  geom_line(data = G1INc_dist, aes(x = Window, y = Dist), color = "red") +
-  geom_line(data = G1OUTa_dist, aes(x = Window, y = Dist), color = "darkred") +
-  geom_line(data = G1OUTb_dist, aes(x = Window, y = Dist), color = "darkred") +
-  geom_line(data = G1OUTc_dist, aes(x = Window, y = Dist), color = "darkred") +
-  geom_line(data = G2INa_dist, aes(x = Window, y = Dist), color = "blue") +
-  geom_line(data = G2INb_dist, aes(x = Window, y = Dist), color = "blue") +
-  geom_line(data = G2INc_dist, aes(x = Window, y = Dist), color = "blue") +
-  geom_line(data = G2OUTa_dist, aes(x = Window, y = Dist), color = "midnightblue") +
-  geom_line(data = G2OUTb_dist, aes(x = Window, y = Dist), color = "midnightblue") +
-  geom_line(data = G2OUTc_dist, aes(x = Window, y = Dist), color = "midnightblue") +
-  ylim(0, 0.3) +
+  geom_line(data = G1INa_dist, aes(x = Window.Start, y = Dist), color = "red") +
+  geom_line(data = G1INb_dist, aes(x = Window.Start, y = Dist), color = "red") +
+  geom_line(data = G1INc_dist, aes(x = Window.Start, y = Dist), color = "red") +
+  geom_line(data = G1OUTa_dist, aes(x = Window.Start, y = Dist), color = "darkred") +
+  geom_line(data = G1OUTb_dist, aes(x = Window.Start, y = Dist), color = "darkred") +
+  geom_line(data = G1OUTc_dist, aes(x = Window.Start, y = Dist), color = "darkred") +
+  geom_line(data = G2INa_dist, aes(x = Window.Start, y = Dist), color = "blue") +
+  geom_line(data = G2INb_dist, aes(x = Window.Start, y = Dist), color = "blue") +
+  geom_line(data = G2INc_dist, aes(x = Window.Start, y = Dist), color = "blue") +
+  geom_line(data = G2OUTa_dist, aes(x = Window.Start, y = Dist), color = "midnightblue") +
+  geom_line(data = G2OUTb_dist, aes(x = Window.Start, y = Dist), color = "midnightblue") +
+  geom_line(data = G2OUTc_dist, aes(x = Window.Start, y = Dist), color = "midnightblue") +
+  ylim(0, 0.35) +
   xlab("M") +
   ylab("Distance to basal group")
 
@@ -452,8 +452,9 @@ Break2 <- data.frame(
 )
 
 SrcGen <- data.frame(
-  Window.Start.Site = Break2$Window-20,
-  Window.End.Site = (Break2$Window-20)+602,
+  Window.Start.Site = Break2$Window,
+  Window.End.Site = Break2$Window+round((length(G2a.seqs)/2)*0.07),
   Lineage = ifelse(Break2$Dist > 0, "LineageA", "LineageB")
 )
+
 SrcGen
